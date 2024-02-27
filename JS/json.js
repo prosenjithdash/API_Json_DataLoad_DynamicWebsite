ee2c09d693ce1fde271d528fea3fea21d9606902
prosenjithdashpappu <prosenjith.edu@gmail.com>
2024-02-28 02:57:24 +0600

    JSON Stringify and parse done

--- JS/json.js
+++ JS/json.js
@@ -17,16 +17,19 @@ Server gave your data follow some way
 
 
 
-const user = { name: 'pappu', id: '14749', dip: 'cse' } // Object Data
+const user = { name: 'pappu', id: '14749', dip: 'cse' } // JavaScript Object Data
 
-// Convert Object Data To JSON String Data
+// Convert JavaScript Object Data To JSON String Data
 
-const stringified = JSON.stringify(user) // String Data
-console.log('Convert Object Data To JSON String Data, Now type of object to',typeof (stringified))
+const stringified = JSON.stringify(user) // JSON String Data
+console.log('Convert JavaScript Object Data To JSON String Data, Now type of JavaScript object to', typeof (stringified))
+console.log('Data = ', stringified)
 
 
-// Convert JSON String Data To Object Data
+// Convert JSON String Data To JavaScript Object Data
 
 const StringToParse = JSON.parse(stringified);
-console.log('Convert JSON String Data To Object Data, Now type of string to',typeof (StringToParse))
+console.log('Convert JSON String Data To JavaScript Object Data, Now type of string to', typeof (StringToParse))
+console.log('Data = ', StringToParse)
+
 
